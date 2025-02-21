8a42d7331882918587a0b8f1225a546237063670
Areyes116 <adrian.reyes2006@icloud.com>
2025-02-21 05:15:14 +0000

    Arise Complete

--- Main.java
+++ Main.java
@@ -3,71 +3,61 @@ import java.util.Scanner;
 
 public class Main
 {
-  public static void main(String[] args)
-  {
-    Integer X = new Integer(5);
-    Integer X = new Integer(10);
-    int z = X.intValue() + Y.intValue();
-
-    ArrayList<Integers> foobar = new ArrayList<Integer>();
-    foobar.add(new Integer(1));
-    foobar.add(new Integer(2));
-    foobar.add(new Integer(3));
-    foobar.add(new Integer(4));
-    foobar.add(new Integer(5));
-    foobar.add(new Integer(6));
-
-    for (int i = 0; i < foobar.size(); i++)
+    public static void main(String[] args)
     {
-      System.out.println(foobar.get(i));
+        Scanner sc = new Scanner(System.in);
+        ArrayList<String> words = new ArrayList<>();
+        System.out.println("Please enter words, enter STOP to stop the loop.");
+        
+        while (true)
+        {
+            String input = sc.nextLine();
+            if (input.equals("STOP")) {
+                break;
+            }
+            words.add(input);
+        }
+        
+        // Print ArrayList
+        System.out.println(words);
+        
+        // Print words in the required pattern
+        for (int i = words.size() - 1; i >= 0; i--)
+        {
+            System.out.println(words.get(i) + words.get(words.size() - 1 - i));
+        }
     }
-
-    int i = 0;
-    while (i < foobar.size())
-    {
-      System.out.println(foobar.get(i));\
-      i++
-    }
-
-    for (Integer N : foobar)
-    {
-      System.out.println(N);
-      System.out.println(N.intValue());
-    }
-
-    /**make an array for ints, fill it with some values, loop through it
-     * print everyelement
-     */
-    int[] foo = {8,6,7,5,3,0,9};
-    int[] bar = new int[5];
-    bar[2] = 8; //etc
-
-    for (int i = 0; i < foo.lenght; i++)
-    {
-      System.out.println(foo[i]);
-    }
-
     
-
-
-    Scanner sc = new Scanner(System.in);
-    String input = "";
-
-    while (!input.equals("STOP"))
+    // Problem 2 - HighestNum
+    public static Integer highestNum(ArrayList<Integer> arr)
     {
-      System.out.println("infinite loop! replace with your code");
+        if (arr.isEmpty())
+        {
+            return null; // Return null if the list is empty
+        }
+        
+        int max = arr.get(0);
+        for (int num : arr)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        return max;
     }
-  }
-
-  // Problem 2 - HighestNum
-  public static Integer highestNum(ArrayList<Integer> arr)
-  {
-    return null;
-  }
-
-  // Problem 3 - getEvens
-  public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
-  {
-    return null;
-  }
+    
+    // Problem 3 - getEvens
+    public static ArrayList<Integer> getEvens(final ArrayList<Integer> arr)
+    {
+        ArrayList<Integer> evens = new ArrayList<>();
+        for (Integer num : arr)
+        {
+            if (num % 2 == 0)
+            {
+                evens.add(num);
+            }
+        }
+        return evens;
+    }
 }
